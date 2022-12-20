f9295231ca75a3005067dee64ba31d04d767899d
Alex W <amwiederman15@gmail.com>
2022-12-19 21:52:45 -0500

    cannot figure out how to dynamically display youtube player

--- script.js
+++ script.js
@@ -1,62 +1,73 @@
-function createVideoPlayer () {
-// 2. This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
+function createVideoPlayer() {
+  // 2. This code loads the IFrame Player API code asynchronously.
+  var tag = document.createElement('script');
 
-tag.src = "https://www.youtube.com/iframe_api";
+  tag.src = "https://www.youtube.com/iframe_api";
 
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
-var player;
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    height: '390',
-    width: '640',
-    videoId: 'iszwuX1AK6A',
-    playerVars: {
-      'playsinline': 1
-    },
-    events: {
-      'onReady': onPlayerReady,
-    //   'onStateChange': onPlayerStateChange
-    }
-  });
-}
+  // 3. This function creates an <iframe> (and YouTube player)
+  //    after the API code downloads.
+  var player;
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: '390',
+      width: '640',
+      videoId: 'video',  //'iszwuX1AK6A',
+      playerVars: {
+        'playsinline': 1
+      },
+      events: {
+        'onReady': onPlayerReady,
+        //   'onStateChange': onPlayerStateChange
+      }
+    });
+  }
 
- // 4. The API will call this function when the video player is ready.
- function onPlayerReady(event) {
+  // 4. The API will call this function when the video player is ready.
+  function onPlayerReady(event) {
     event.target.playVideo();
   }
 };
 
 
 
-  var apiKey = "AIzaSyDZ76_4xh5c5tRRPhgt1pQyPC5dxAdj3T4"
+var apiKey = "AIzaSyDZ76_4xh5c5tRRPhgt1pQyPC5dxAdj3T4"
 var searchQuery = 'Wolf of Wall Street';
 // YouTube API key
-var API_KEY = 'AIzaSyDZ76_4xh5c5tRRPhgt1pQyPC5dxAdj3T4';
+// var API_KEY = 'AIzaSyDZ76_4xh5c5tRRPhgt1pQyPC5dxAdj3T4'; //amwiederman15
+var API_KEY = 'AIzaSyBWiJO8Tjg4qX7p5wfAh4ih4nq6XeKqIAk'; //cemti
 
 // Replace <SEARCH_QUERY> with the keywords or phrases that you want to search for
-var SEARCH_QUERY = 'Wolf of Wall Street' + ' movie trailer';
+var SEARCH_QUERY;
+var video;
 
-function searchMovieId() {
-// Make a search request to the YouTube API
-fetch(`https://www.googleapis.com/youtube/v3/search?part=id&q=${SEARCH_QUERY}&type=video&maxResults=10&key=${API_KEY}`)
-  .then(response => response.json())
-  .then(data => {
-    // Extract the list of videos from the response
-    const videos = data.items[0].id.videoId;
-console.log(videos)
-  });
+function searchMovieId(video) {
+  // Make a search request to the YouTube API
+  fetch(`https://www.googleapis.com/youtube/v3/search?part=id&q=${SEARCH_QUERY}&type=video&maxResults=10&key=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+      // Extract the list of videos from the response
+      video = data.items[0].id.videoId;
+      console.log(video)
+    });
+
 }
 
-var searchEl = document.querySelector(".search")
+var searchEl = document.querySelector(".search");
+var movieEl = document.querySelector(".input");
+var movie;
 
 searchEl.addEventListener("click", () => {
 
-  searchMovieId()
+  movie = movieEl.value;
+  movieEl.value = "";
+  SEARCH_QUERY = movie + ' movie trailer';
+
+  searchMovieId(video) //getting back video id
+
+  createVideoPlayer()
 })
 
 
